5802d142f68d78963c282f0dc6de045a717944ee
Matt Rogge <5900418+mrrogge@users.noreply.github.com>
2021-09-09 09:56:16 -0500

    add more ops to EntityId

--- src/heat/ecs/EntityId.hx
+++ src/heat/ecs/EntityId.hx
@@ -20,7 +20,7 @@ to Either<String, Int> {
     @:to
     inline public function toString():String {
         return switch (this) {
-            case Left(s): 'ID($s)';
+            case Left(s): 'ID("$s")';
             case Right(i): 'ID($i)';
         }
     }
@@ -42,6 +42,63 @@ to Either<String, Int> {
             case Right(a): switch b{
                 case Left(b): false;
                 case Right(b): a == b;
+            }
+        }
+    }
+
+    @:op(a != b) public static inline function neq(a:EntityId, b:EntityId):Bool {
+        return !eq(a, b);
+    }
+
+    //For the comparison operators, we need to define how int IDs relate to string IDs. We will use the convention that all int values are less than all string values.
+    @:op(a < b) public static inline function less(a:EntityId, b:EntityId):Bool {
+        return switch a {
+            case Left(a): switch b {
+                case Left(b): a < b;
+                case Right(b): false;
+            }
+            case Right(a): switch b {
+                case Left(b): true;
+                case Right(b): a < b;
+            }
+        }
+    }
+
+    @:op(a <= b) public static inline function leq(a:EntityId, b:EntityId):Bool {
+        return switch a {
+            case Left(a): switch b {
+                case Left(b): a <= b;
+                case Right(b): false;
+            }
+            case Right(a): switch b {
+                case Left(b): true;
+                case Right(b): a <= b;
+            }
+        }
+    }
+
+    @:op(a > b) public static inline function grt(a:EntityId, b:EntityId):Bool {
+        return switch a {
+            case Left(a): switch b {
+                case Left(b): a > b;
+                case Right(b): true;
+            }
+            case Right(a): switch b {
+                case Left(b): false;
+                case Right(b): a > b;
+            }
+        }
+    }
+
+    @:op(a >= b) public static inline function geq(a:EntityId, b:EntityId):Bool {
+        return switch a {
+            case Left(a): switch b {
+                case Left(b): a >= b;
+                case Right(b): true;
+            }
+            case Right(a): switch b {
+                case Left(b): false;
+                case Right(b): a >= b;
             }
         }
     }

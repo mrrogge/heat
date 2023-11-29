f0292a3ef8590ed753b308f172bd4c611016ca52
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-11-29 16:50:23 -0600

    test moving parent/children

--- examples/SimplePlatformer/SimplePlatformer.hx
+++ examples/SimplePlatformer/SimplePlatformer.hx
@@ -15,19 +15,26 @@ class SimplePlatformer {
         camTX.initTranslate(0, 0);
         space.com.camera.set(camId, new heat.core.Camera());
         space.com.drawOrder.set(camId, 0);
-        space.com.absPosTransform.set(camId, camTX);        
+        space.com.transform.set(camId, camTX);
+        space.com.absPosTransform.set(camId, camTX.clone());
 
         final squareId1 = space.getNextID();
         final tx1 = new heat.core.MTransform();
         tx1.initTranslate(10, 10);
-        space.com.absPosTransform.set(squareId1, tx1);
+        space.com.transform.set(squareId1, tx1);
+        space.com.absPosTransform.set(squareId1, tx1.clone());
         space.com.drawOrder.set(squareId1, 0);
+        space.com.names.set(squareId1, "thing1");
 
         final squareId2 = space.getNextID();
         final tx2 = new heat.core.MTransform();
         tx2.initTranslate(50, 50);
-        space.com.absPosTransform.set(squareId2, tx2);
+        space.com.transform.set(squareId2, tx2);
+        final absTx2 = new heat.core.MTransform();
+        space.com.absPosTransform.set(squareId2, absTx2);
         space.com.drawOrder.set(squareId2, 0);
+        space.com.names.set(squareId2, "thing2");
+        space.setParent(squareId2, squareId1);
     }
 }
 
@@ -41,17 +48,29 @@ class MyHeatSpace extends HeatSpace {
 
     override function onKeyPressed(keyCode:KeyCode) {
         switch (keyCode) {
+            case A: {
+                moveThing("thing1", -20., 0.);
+            }
+            case D: {
+                moveThing("thing1", 20., 0.);
+            }
+            case W: {
+                moveThing("thing1", 0., -20.);
+            }
+            case S: {
+                moveThing("thing1", 0., 20.);
+            }
             case LEFT: {
-                translateCamera(-20., 0.);
+                moveThing("thing2", -20., 0.);
             }
             case RIGHT: {
-                translateCamera(20., 0.);
+                moveThing("thing2", 20., 0.);
             }
             case UP: {
-                translateCamera(0., -20.);
+                moveThing("thing2", 0., -20.);
             }
             case DOWN: {
-                translateCamera(0., 20.);
+                moveThing("thing2", 0., 20.);
             }
             default: {}
         }
@@ -63,6 +82,16 @@ class MyHeatSpace extends HeatSpace {
         for (id in cameraQuery.result) {
             com.absPosTransform.get(id).x += x;
             com.absPosTransform.get(id).y += y;
+        }
+    }
+
+    function moveThing(name: String, x: Float, y: Float) {
+        final query = new ComQuery().whereEqualTo(com.names, name)
+            .with(com.transform);
+        query.run();
+        for (id in query.result) {
+            com.transform.get(id).x += x;
+            com.transform.get(id).y += y;
         }
     }
 }

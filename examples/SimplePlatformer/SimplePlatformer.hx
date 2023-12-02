7722043ce4f6adfdc116ab80876f378624a9f36a
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-12-01 23:19:55 -0600

    move game logic into separate space class

--- examples/SimplePlatformer/SimplePlatformer.hx
+++ examples/SimplePlatformer/SimplePlatformer.hx
@@ -5,95 +5,12 @@ class SimplePlatformer {
         new SimplePlatformer();
     }
 
-    var space = new MyHeatSpace();
-    var bridge = new heat.bridges.heaps.HeapsBridge();
+    final bridge:heat.bridges.heaps.HeapsBridge;
 
     public function new() {
-        bridge.attach(space);
-        final camId = space.getNextID();
-        final camTX = new heat.core.MTransform();
-        camTX.initTranslate(0, 0);
-        space.com.camera.set(camId, new heat.core.Camera());
-        space.com.drawOrder.set(camId, 0);
-        space.com.transform.set(camId, camTX);
-        space.com.absPosTransform.set(camId, camTX.clone());
-
-        final squareId1 = space.getNextID();
-        final tx1 = new heat.core.MTransform();
-        tx1.initTranslate(10, 10);
-        space.com.transform.set(squareId1, tx1);
-        space.com.absPosTransform.set(squareId1, tx1.clone());
-        space.com.drawOrder.set(squareId1, 0);
-        space.com.names.set(squareId1, "thing1");
-        space.com.textureRegions.set(squareId1, new heat.texture.TextureRegion(heat.texture.TextureHandle.Color(heat.core.Color.RGB(1, 0, 0)), 0, 0, 32, 32));
-
-        final squareId2 = space.getNextID();
-        final tx2 = new heat.core.MTransform();
-        tx2.initTranslate(50, 50);
-        space.com.transform.set(squareId2, tx2);
-        final absTx2 = new heat.core.MTransform();
-        space.com.absPosTransform.set(squareId2, absTx2);
-        space.com.drawOrder.set(squareId2, 0);
-        space.com.names.set(squareId2, "thing2");
-        space.setParent(squareId2, squareId1);
-        space.com.textureRegions.set(squareId2, new heat.texture.TextureRegion(heat.texture.TextureHandle.Color(heat.core.Color.RGB(0, 1, 0)), 0, 0, 64, 64));
-    }
-}
-
-class MyHeatSpace extends HeatSpace {
-    final cameraQuery = new ComQuery();
-
-    public function new() {
-        super();
-        cameraQuery.with(com.camera).with(com.absPosTransform);
-    }
-
-    override function onKeyPressed(keyCode:KeyCode) {
-        switch (keyCode) {
-            case A: {
-                moveThing("thing1", -20., 0.);
-            }
-            case D: {
-                moveThing("thing1", 20., 0.);
-            }
-            case W: {
-                moveThing("thing1", 0., -20.);
-            }
-            case S: {
-                moveThing("thing1", 0., 20.);
-            }
-            case LEFT: {
-                moveThing("thing2", -20., 0.);
-            }
-            case RIGHT: {
-                moveThing("thing2", 20., 0.);
-            }
-            case UP: {
-                moveThing("thing2", 0., -20.);
-            }
-            case DOWN: {
-                moveThing("thing2", 0., 20.);
-            }
-            default: {}
-        }
-        trace(keyCode);
-    }
-
-    function translateCamera(x: Float, y:Float) {
-        cameraQuery.run();
-        for (id in cameraQuery.result) {
-            com.absPosTransform.get(id).x += x;
-            com.absPosTransform.get(id).y += y;
-        }
-    }
-
-    function moveThing(name: String, x: Float, y: Float) {
-        final query = new ComQuery().whereEqualTo(com.names, name)
-            .with(com.transform);
-        query.run();
-        for (id in query.result) {
-            com.transform.get(id).x += x;
-            com.transform.get(id).y += y;
-        }
+        bridge = new heat.bridges.heaps.HeapsBridge(() -> {
+            final space = new SimplePlatformerSpace();
+            bridge.attach(space);
+        });
     }
 }
---
+++ examples/SimplePlatformer/SimplePlatformerSpace.hx
@@ -0,0 +1,77 @@
+using heat.HeatPrelude;
+
+@:autoBuild
+@:build(heat.core.macro.HeatSpaceMacro.applyGamePlugin())
+class SimplePlatformerSpace extends HeatSpace {
+    final cameraQuery = new ComQuery();
+    final transformQuery = new ComQuery();
+    final heroQuery = new ComQuery();
+
+    public function new() {
+        super();
+
+        cameraQuery.with(com.camera).with(com.absPosTransform);
+        transformQuery.with(com.transform);
+        heroQuery.with(heroMap);
+
+        final camId = getNextID();
+        final camTX = new heat.core.MTransform();
+        camTX.initTranslate(0, 0);
+        com.camera.set(camId, new heat.core.Camera());
+        com.drawOrder.set(camId, 0);
+        com.transform.set(camId, camTX);
+        com.absPosTransform.set(camId, camTX.clone());
+
+        final heroID = getNextID();
+        final tx1 = new heat.core.MTransform();
+        tx1.initTranslate(10, 10);
+        com.transform.set(heroID, tx1);
+        com.absPosTransform.set(heroID, tx1.clone());
+        com.drawOrder.set(heroID, 0);
+        com.textureRegions.set(heroID, new heat.texture.TextureRegion(
+            heat.texture.TextureHandle.Other(hxd.Res.girl_png), 0, 0, 32, 48
+        ));
+        heroMap.set(heroID, Noise);
+
+    }
+
+    override function onKeyPressed(keyCode:KeyCode) {
+        switch (keyCode) {
+            case A, LEFT: {
+                moveHero(-20., 0.);
+            }
+            case D, RIGHT: {
+                moveHero(20., 0.);
+            }
+            case W, UP: {
+                moveHero(0., -20.);
+            }
+            case S, DOWN: {
+                moveHero(0., 20.);
+            }
+            default: {}
+        }
+        trace(keyCode);
+    }
+
+    function translateCamera(x: Float, y:Float) {
+        cameraQuery.run();
+        for (id in cameraQuery.result) {
+            com.absPosTransform.get(id).x += x;
+            com.absPosTransform.get(id).y += y;
+        }
+    }
+
+    function moveThing(id: EntityId, x: Float, y: Float) {
+        if (!transformQuery.checkId(id)) return;
+        com.transform.get(id).x += x;
+        com.transform.get(id).y += y;
+    }
+
+    function moveHero(x: Float, y: Float) {
+        heroQuery.run();
+        for (id in heroQuery.result) {
+            moveThing(id, x, y);
+        }
+    }
+}

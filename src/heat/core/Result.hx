16a9d5299cbc4af7562e9d9423093c64fb5ed486
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-12-09 16:12:18 -0600

    add method for resolving arrays of results

--- src/heat/core/Result.hx
+++ src/heat/core/Result.hx
@@ -1,6 +1,24 @@
 package heat.core;
 
+@:using(heat.core.Result.ResultTools)
 enum Result<TSuccess, TFailure> {
-    Ok(result:TSuccess);
-    Err(err:TFailure);
+	Ok(result:TSuccess);
+	Err(err:TFailure);
+}
+
+class ResultTools {
+	public static function all<TSuccess, TFailure>(cls:Enum<Result<TSuccess, TFailure>>, results:Array<Result<TSuccess, TFailure>>):Result<Array<TSuccess>, TFailure> {
+        final successArray:Array<TSuccess> = [];
+        for (result in results) {
+            switch (result) {
+                case Ok(result): {
+                    successArray.push(result);
+                }
+                case Err(err): {
+                    return Err(err);
+                }
+            }
+        }
+        return Ok(successArray);
+    }
 }

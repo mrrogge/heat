261c78f55289c506214e9ef87efa3fbd6060c9c2
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-12-13 20:50:21 -0600

    simplify plugin, add more coms

--- examples/SimplePlatformer/SimplePlatformerPlugin.hx
+++ examples/SimplePlatformer/SimplePlatformerPlugin.hx
@@ -1,52 +1,23 @@
 import haxe.macro.Expr;
 import haxe.macro.Context;
 
+using heat.core.PluginTools;
+
+#if (macro || eval)
 class SimplePlatformerPlugin {
-    public static macro function build():Array<Field> {
-        var fields = Context.getBuildFields();
-        final comMapExprs = macro [
-            heroMap is heat.core.Noise,
-            colliders is heat.core.Circle,
-        ];
-        final newFields = switch (heat.core.macro.HeatSpaceMacro.makeComMapFields(comMapExprs, true)) {
-            case Err(err): {
-                return Context.error(err, Context.currentPos());
-            }
-            case Ok(newFields): newFields;
-        }
-
-        fields = fields.concat(newFields);
-
-        return fields;
-    }
-
-    #if (macro || eval)
-    @:keep
-    public static function init():Void {
-        Context.onAfterInitMacros(() -> {            
-            final comMapExprs = macro [
-                heroMap is heat.core.Noise,
-                colliders is heat.core.Circle,
-            ];
-
-            final interfaceFields = switch (heat.core.macro.HeatSpaceMacro.makeComMapFields(comMapExprs, false)) {
-                case Err(err): {
-                    Context.error(err, Context.currentPos());
-                    return;
-                }
-                case Ok(newFields): newFields;
-            }
-            
-            var interfaceType:TypeDefinition = {
-                pack: [],
-                name: 'I_SimplePlatformerSpace',
-                pos: Context.currentPos(),
-                kind: TDClass(null, null, true, false, false),
-                fields: interfaceFields,
-            }
     
-            Context.defineType(interfaceType);
-        });
+    public static macro function apply():Array<Field> {
+        return SimplePlatformerPlugin.applyWrapper(comMapExprs, "game");
     }
-    #end
+
+    public static function init():Void {
+        SimplePlatformerPlugin.initWrapper(comMapExprs, "I_UsesSimplePlatformerPlugin", "game");
+    }
+
+    static final comMapExprs = macro [
+        heroMap is heat.core.Noise,
+        colliders is heat.core.Circle,
+        heroMoveStates is HeroMoveState,
+    ];
 }
+#end

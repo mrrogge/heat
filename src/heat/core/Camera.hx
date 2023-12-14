24eb1e66b1e0b4bce0cf2e7eeb67ce1c479465e0
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-12-13 20:38:29 -0600

    add camera filter function

--- src/heat/core/Camera.hx
+++ src/heat/core/Camera.hx
@@ -1,7 +1,13 @@
 package heat.core;
 
+using heat.HeatPrelude;
+
 class Camera {
     public var scale = 1.;
 
     public function new() {}
+
+    public dynamic function idFilter(id: EntityId):Bool {
+        return true;
+    }
 }

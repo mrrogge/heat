a3087ec8105e8a40cc568d1e1ab73de56f8df28e
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-12-27 19:45:01 -0600

    cleanup example w/ bundles

--- examples/SimplePlatformer/SimplePlatformerSpace.hx
+++ examples/SimplePlatformer/SimplePlatformerSpace.hx
@@ -6,6 +6,27 @@ class SimplePlatformerSpace extends heat.std.HeatSpaceStd {
 	final transformQuery = new ComQuery();
 	final heroQuery = new ComQuery();
 
+	function makeCameraBundle(id:EntityId, template:heat.ecs.BundleTemplate<{x:Float, y:Float}> = DEFAULT) {
+		makeTransformBundle(id, template);
+		com.camera.set(id, new heat.core.Camera());
+		com.drawOrder.set(id, 0);
+	}
+
+	function makeTransformBundle(id:EntityId, template:heat.ecs.BundleTemplate<{x:Float, y:Float}> = DEFAULT) {
+		final tx = new MTransform();
+		com.transform.set(id, tx);
+		com.absPosTransform.set(id, tx.clone());
+		switch (template) {
+			case DEFAULT:
+				{}
+			case CUSTOM(template):
+				{
+					tx.x = template.x;
+					tx.y = template.y;
+				}
+		}
+	}
+
 	public function new() {
 		super();
 
@@ -13,27 +34,34 @@ class SimplePlatformerSpace extends heat.std.HeatSpaceStd {
 		transformQuery.with(com.transform);
 		heroQuery.with(game.heroMap);
 
-		final camId = getNextID();
-		final camTX = new heat.core.MTransform();
-		camTX.initTranslate(0, 0);
-		com.camera.set(camId, new heat.core.Camera());
-		com.drawOrder.set(camId, 0);
-		com.transform.set(camId, camTX);
-		com.absPosTransform.set(camId, camTX.clone());
+		final worldCamId = getNextID();
+		makeCameraBundle(worldCamId);
+		final worldCamFilterQuery = new ComQuery().with(game.worldObjects);
+		com.camera.get(worldCamId).idFilter = (id:EntityId) -> {
+			return worldCamFilterQuery.checkId(id);
+		};
+
+		final uiCamId = getNextID();
+		makeCameraBundle(uiCamId);
+		final uiCamFilterQuery = new ComQuery().with(game.uiObjects);
+		com.camera.get(uiCamId).idFilter = (id:EntityId) -> {
+			return uiCamFilterQuery.checkId(id);
+		};
 
 		final heroID = getNextID();
-		final tx1 = new heat.core.MTransform();
-		tx1.initTranslate(10, 10);
-		com.transform.set(heroID, tx1);
-		com.absPosTransform.set(heroID, tx1.clone());
+		makeTransformBundle(heroID, CUSTOM({x: 10, y: 10}));
 		com.drawOrder.set(heroID, 0);
-		com.textureRegions.set(heroID, new heat.texture.TextureRegion(heat.texture.TextureHandle.Other(hxd.Res.girl_png), 0, 0, 32, 48));
+		final heroTexture = new TextureRegion(None, 0, 0, 32, 48);
+		com.textureRegions.set(heroID, heroTexture);
+		heroTexture.handle = TextureHandle.Other(hxd.Res.girl_png);
+		heroTexture.center();
 		game.heroMap.set(heroID, Noise);
 		game.heroMoveStates.set(heroID, new HeroMoveState());
 	}
 
 	override function update(dt:Float) {
 		HeroSys.update(this, dt);
+		heat.ui.FlexBoxSys.update(this, dt);
 		syncAbsPos();
 	}
 

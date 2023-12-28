67fb77f78de406f3cefc77fde4f91fd8315a8506
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-12-27 21:19:51 -0600

    fix issues in camera sys

--- src/heat/camera/CameraSys.hx
+++ src/heat/camera/CameraSys.hx
@@ -3,9 +3,9 @@ package heat.camera;
 using heat.HeatPrelude;
 
 class CameraSys {
-    function makeCameraBundle(space:heat.I_UsesHeatStandardPlugin, id:EntityId, template:heat.ecs.BundleTemplate<{x:Float, y:Float}> = DEFAULT) {
+    public static function makeCameraBundle(space:heat.I_UsesHeatStandardPlugin, id:EntityId, template:heat.ecs.BundleTemplate<{x:Float, y:Float}> = DEFAULT) {
 		heat.transform.TransformSys.makeTransformBundle(space, id, template);
-		space.com.camera.set(id, new heat.core.Camera());
+		space.com.camera.set(id, new heat.camera.Camera());
 		space.com.drawOrder.set(id, 0);
 	}
 }

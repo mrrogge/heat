18b7b8b702c7ef4fb01e2d4207d4a13b267e3d53
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-12-27 19:48:57 -0600

    minor speed changes

--- examples/SimplePlatformer/HeroMoveState.hx
+++ examples/SimplePlatformer/HeroMoveState.hx
@@ -1,18 +1,16 @@
 using heat.HeatPrelude;
 
 class HeroMoveState {
-    public var leftCmd = false;
-    public var rightCmd = false;
-    public var upCmd = false;
-    public var downCmd = false;
-    public var dragMult = 0.1;
-    public final vel = new MVectorFloat2();
-    public final prevVel = new MVectorFloat2();
-    public var topSpeed = 100.; 
-    public var timeToTopSpeedSec = 0.5;
-    public var timeToStopSec = 0.2;
+	public var leftCmd = false;
+	public var rightCmd = false;
+	public var upCmd = false;
+	public var downCmd = false;
+	public var dragMult = 0.1;
+	public final vel = new MVectorFloat2();
+	public final prevVel = new MVectorFloat2();
+	public var topSpeed = 100.;
+	public var timeToTopSpeedSec = 0.15;
+	public var timeToStopSec = 0.15;
 
-    public function new() {}
-
-
+	public function new() {}
 }

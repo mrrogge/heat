7495c990180c29f54a635e10f7a47f9f2002cf40
mrrogge <5900418+mrrogge@users.noreply.github.com>
2024-01-03 17:31:30 -0600

    formatting

--- src/heat/transform/TransformSys.hx
+++ src/heat/transform/TransformSys.hx
@@ -3,7 +3,8 @@ package heat.transform;
 using heat.HeatPrelude;
 
 class TransformSys {
-    public static function makeTransformBundle(space:heat.I_UsesHeatStandardPlugin, id:EntityId, template:heat.ecs.BundleTemplate<{x:Float, y:Float}> = DEFAULT) {
+	public static function makeTransformBundle(space:heat.I_UsesHeatStandardPlugin, id:EntityId,
+			template:heat.ecs.BundleTemplate<{x:Float, y:Float}> = DEFAULT) {
 		final tx = new MTransform();
 		space.com.transform.set(id, tx);
 		space.com.absPosTransform.set(id, tx.clone());

dea99d73b51899ce544f7566920ff7a009111de9
mrrogge <5900418+mrrogge@users.noreply.github.com>
2024-01-07 18:54:03 -0600

    .

--- examples/SimplePlatformer/SimplePlatformer.hx
+++ examples/SimplePlatformer/SimplePlatformer.hx
@@ -1,16 +1,16 @@
 using heat.HeatPrelude;
 
 class SimplePlatformer {
-    public static function main() {
-        new SimplePlatformer();
-    }
+	public static function main() {
+		new SimplePlatformer();
+	}
 
-    final bridge:heat.bridges.heaps.HeapsBridge;
+	final bridge:heat.bridges.heaps.HeapsBridge;
 
-    public function new() {
-        bridge = new heat.bridges.heaps.HeapsBridge(() -> {
-            final space = new SimplePlatformerSpace();
-            bridge.attach(space, space);
-        });
-    }
+	public function new() {
+		bridge = new heat.bridges.heaps.HeapsBridge(() -> {
+			final space = new SimplePlatformerSpace();
+			bridge.attach(space);
+		});
+	}
 }

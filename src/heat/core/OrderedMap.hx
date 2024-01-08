6c00283a5055c282b550ef55fb50f96749d9c8ce
mrrogge <5900418+mrrogge@users.noreply.github.com>
2024-01-07 18:43:36 -0600

    comment out for now

--- src/heat/core/OrderedMap.hx
+++ src/heat/core/OrderedMap.hx
@@ -1,55 +1,45 @@
 package heat.core;
 
-class OrderedMap<K, V> {
-	final map:Map<K, Int> = [];
-	final list:Array<V> = [];
-
-	public function new() {}
-
-	public function get(k:K):Null<V> {
-		return map[k] == null ? null : list[map[k]];
-	}
-
-	public function set(k:K, v:V):Void {
-		// TODO
-	}
-
-	function exists(k:K):Bool {
-		// TODO
-		return false;
-	};
-
-	function remove(k:K):Bool {
-		// TODO
-		return false;
-	}
-
-	function keys():Iterator<K> {
-		// TODO
-		return null;
-	}
-
-	function iterator():Iterator<V> {
-		// TODO
-		return null;
-	}
-
-	function keyValueIterator():KeyValueIterator<K, V> {
-		// TODO
-		return null;
-	}
-
-	function copy():haxe.Constraints.IMap<K, V> {
-		// TODO
-		return null;
-	}
-
-	function toString():String {
-		// TODO
-		return "";
-	}
-
-	function clear():Void {
-		// TODO
-	}
-}
+// TODO: needs work. Will need to be an abstract like haxe.ds.Map so the inner map properly handles the underlying types involved.
+// class OrderedMap<K, V> {
+// 	final map:Map<K, Int> = [];
+// 	final list:Array<V> = [];
+// 	public function new() {}
+// 	public function get(k:K):Null<V> {
+// 		return map[k] == null ? null : list[map[k]];
+// 	}
+// 	public function set(k:K, v:V):Void {
+// 		// TODO
+// 	}
+// 	function exists(k:K):Bool {
+// 		// TODO
+// 		return false;
+// 	};
+// 	function remove(k:K):Bool {
+// 		// TODO
+// 		return false;
+// 	}
+// 	function keys():Iterator<K> {
+// 		// TODO
+// 		return null;
+// 	}
+// 	function iterator():Iterator<V> {
+// 		// TODO
+// 		return null;
+// 	}
+// 	function keyValueIterator():KeyValueIterator<K, V> {
+// 		// TODO
+// 		return null;
+// 	}
+// 	function copy():haxe.Constraints.IMap<K, V> {
+// 		// TODO
+// 		return null;
+// 	}
+// 	function toString():String {
+// 		// TODO
+// 		return "";
+// 	}
+// 	function clear():Void {
+// 		// TODO
+// 	}
+// }

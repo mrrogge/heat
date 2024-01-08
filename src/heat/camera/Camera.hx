2afcb910265e36d56a531a12639f65b5d9e8dd43
mrrogge <5900418+mrrogge@users.noreply.github.com>
2024-01-07 18:50:46 -0600

    don't use prelude

--- src/heat/camera/Camera.hx
+++ src/heat/camera/Camera.hx
@@ -1,11 +1,11 @@
 package heat.camera;
 
-using heat.HeatPrelude;
+import heat.ecs.EntityId;
 
 class Camera {
-    public var scale = 1.;
-    public var viewWidth = 800;
-    public var viewHeight = 600;
+	public var scale = 1.;
+	public var viewWidth = 800;
+	public var viewHeight = 600;
 
 	public function new() {}
 

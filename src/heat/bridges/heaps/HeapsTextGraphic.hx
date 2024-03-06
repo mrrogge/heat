1daaced01909bdad49d8b1f79d6390148928d3c8
mrrogge <5900418+mrrogge@users.noreply.github.com>
2024-03-05 20:36:14 -0600

    use font handles in text graphic rendering

--- src/heat/bridges/heaps/HeapsTextGraphic.hx
+++ src/heat/bridges/heaps/HeapsTextGraphic.hx
@@ -14,6 +14,30 @@ class HeapsTextGraphic implements heat.text.ITextGraphic {
 		return this.text;
 	}
 
+	public var fontHandle(default, set):heat.text.FontHandle;
+
+	function set_fontHandle(fontHandle:heat.text.FontHandle):heat.text.FontHandle {
+		switch fontHandle {
+			case File(path):
+				{
+					// TODO
+					heapsText.font = hxd.res.DefaultFont.get();
+				}
+			case Other(other):
+				{
+					if (Std.isOfType(other, h2d.Font)) {
+						final font = (other : h2d.Font);
+						heapsText.font = font;
+					}
+				}
+			case None:
+				{
+					heapsText.font = hxd.res.DefaultFont.get();
+				}
+		}
+		return fontHandle;
+	}
+
 	public function new(heapsText:h2d.Text) {
 		this.heapsText = heapsText;
 	}
--- src/heat/text/ITextGraphic.hx
+++ src/heat/text/ITextGraphic.hx
@@ -2,4 +2,5 @@ package heat.text;
 
 interface ITextGraphic {
 	public var text(get, set):String;
+	public var fontHandle(default, set):FontHandle;
 }

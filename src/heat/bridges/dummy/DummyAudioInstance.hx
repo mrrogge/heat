b02860bf1a5a7909a664c141f641f3e95e4ebef6
mrrogge <5900418+mrrogge@users.noreply.github.com>
2024-03-16 13:51:24 -0500

    updates to dummy bridge

--- src/heat/bridges/dummy/DummyAudioInstance.hx
+++ src/heat/bridges/dummy/DummyAudioInstance.hx
@@ -1,5 +1,7 @@
 package heat.bridges.dummy;
 
+import heat.audio.AudioHandle;
+
 class DummyAudioInstance implements heat.audio.IAudioInstance {
 	public final handle:AudioHandle;
 	public var position(get, set):Float;
@@ -14,7 +16,16 @@ class DummyAudioInstance implements heat.audio.IAudioInstance {
 
 	public var isMuted(get, set):Bool;
 
-	inline function get_isMuted():Bool {}
+	var _isMuted = false;
+
+	inline function get_isMuted():Bool {
+		return _isMuted;
+	}
+
+	function set_isMuted(isMuted:Bool):Bool {
+		_isMuted = isMuted;
+		return _isMuted;
+	}
 
 	public function new(handle:heat.audio.AudioHandle) {
 		this.handle = handle;
@@ -25,10 +36,47 @@ class DummyAudioInstance implements heat.audio.IAudioInstance {
 	}
 
 	public var volume(get, set):Float;
+
+	var _volume = 0.;
+
+	inline function get_volume():Float {
+		return _volume;
+	}
+
+	function set_volume(volume:Float):Float {
+		_volume = volume;
+		return _volume;
+	}
+
 	public var isLooping(get, set):Bool;
+
+	var _isLooping = false;
+
+	inline function get_isLooping():Bool {
+		return _isLooping;
+	}
+
+	function set_isLooping(isLooping:Bool):Bool {
+		_isLooping = isLooping;
+		return _isLooping;
+	}
+
 	public var isPaused(get, set):Bool;
 
-	public function isFinished():Bool;
+	var _isPaused = false;
 
-	public function stop():Void;
+	inline function get_isPaused():Bool {
+		return _isPaused;
+	}
+
+	function set_isPaused(isPaused:Bool):Bool {
+		_isPaused = isPaused;
+		return _isPaused;
+	}
+
+	public function isFinished():Bool {
+		return true;
+	}
+
+	public function stop():Void {}
 }
--- src/heat/bridges/dummy/DummyBridge.hx
+++ src/heat/bridges/dummy/DummyBridge.hx
@@ -2,14 +2,14 @@ package heat.bridges.dummy;
 
 using heat.HeatPrelude;
 
-class DummyBridge implements heat.audio.IAudioBridge {
-	final space:heat.core.HeatSpace;
+class DummyBridge {
+	final space:heat.I_MinimalHeatSpace;
 
-	public function new(space:heat.core.HeatSpace) {
+	public function new(space:heat.I_MinimalHeatSpace, periodMS:Int) {
 		this.space = space;
-	}
-
-	public function playAudio(source:heat.audio.AudioSource):heat.core.Result<heat.audio.IAudioInstance, String> {
-		return new DummyAudioInstance(source.handle);
+		final timer = new haxe.Timer(periodMS);
+		timer.run = () -> {
+			space.update(periodMS / 1000.);
+		}
 	}
 }

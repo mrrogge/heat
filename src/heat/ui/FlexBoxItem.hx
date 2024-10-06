8fa3e11adf5ac68a26dde96aa63f40ec38597aef
mrrogge <5900418+mrrogge@users.noreply.github.com>
2024-10-05 21:15:11 -0500

    flexbox improvements

--- src/heat/ui/FlexBoxItem.hx
+++ src/heat/ui/FlexBoxItem.hx
@@ -1,5 +1,17 @@
 package heat.ui;
 
 class FlexBoxItem {
-	public function new() {}
+	public var flex(default, set) = 1;
+
+	function set_flex(x:Null<Int>):Null<Int> {
+		this.flex = x;
+		if (this.flex < 0) {
+			this.flex = 0;
+		}
+		return this.flex;
+	}
+
+	public function new(flex = 0) {
+		this.flex = flex;
+	}
 }
--- src/heat/ui/FlexBoxSys.hx
+++ src/heat/ui/FlexBoxSys.hx
@@ -15,48 +15,58 @@ class FlexBoxSys {
 		for (id in rootQuery.result) {
 			final fb = space.com.flexBoxes.get(id);
 			final fbDim = space.com.dimensions.get(id);
-			final fbTX = space.com.transform.get(id);
 			final children = space.com.childrenLists.get(id);
 			final childQuery = comQueryPool.get()
 				.whereEqualTo(space.com.parents, id)
 				.with(space.com.flexBoxItems)
 				.with(space.com.dimensions)
 				.with(space.com.transform);
+			var flexTotal = 0;
 			for (i in 0...children.length) {
 				final childId = children[i];
 				if (!childQuery.checkId(childId)) {
 					continue;
 				}
+				final flex = space.com.flexBoxItems.get(childId).flex;
+				if (flex != 0) {
+					flexTotal += flex;
+				}
+			}
+
+			var workingX = 0.;
+			var workingY = 0.;
+			for (i in 0...children.length) {
+				final index = switch (fb.dir) {
+					case ROW, COL: i;
+					case ROW_REVERSE, COL_REVERSE: children.length - 1 - i;
+				}
+				final childId = children[index];
+				if (!childQuery.checkId(childId)) {
+					continue;
+				}
 				final childDim = space.com.dimensions.get(childId);
 				final childTX = space.com.transform.get(childId);
+				final flex = space.com.flexBoxItems.get(childId).flex;
 				switch (fb.dir) {
-					case ROW:
+					case ROW, ROW_REVERSE:
 						{
-							childDim.w = (fbDim.w - (fb.gap * (children.length - 1))) / children.length;
+							if (flex != 0) {
+								childDim.w = (fbDim.w - (fb.gap * (children.length - 1))) * flex / flexTotal;
+							}
 							childDim.h = fbDim.h;
-							childTX.x = (childDim.w + fb.gap) * i;
-							childTX.y = 0;
+							childTX.x = workingX;
+							childTX.y = workingY;
+							workingX += (childDim.w + fb.gap);
 						}
-					case ROW_REVERSE:
+					case COL, COL_REVERSE:
 						{
-							childDim.w = (fbDim.w - (fb.gap * (children.length - 1))) / children.length;
-							childDim.h = fbDim.h;
-							childTX.x = fbDim.w - (childDim.w * (i + 1)) - (fb.gap * i);
-							childTX.y = 0;
-						}
-					case COL:
-						{
-							childDim.h = (fbDim.h - (fb.gap * (children.length - 1))) / children.length;
 							childDim.w = fbDim.w;
-							childTX.y = (childDim.h + fb.gap) * i;
-							childTX.x = 0;
-						}
-					case COL_REVERSE:
-						{
-							childDim.h = (fbDim.h - (fb.gap * (children.length - 1))) / children.length;
-							childDim.w = fbDim.w;
-							childTX.y = fbDim.h - (childDim.h * (i + 1)) - (fb.gap * i);
-							childTX.x = 0;
+							if (flex != 0) {
+								childDim.h = (fbDim.h - (fb.gap * (children.length - 1))) * flex / flexTotal;
+							}
+							childTX.x = workingX;
+							childTX.y = workingY;
+							workingY += (childDim.h + fb.gap);
 						}
 				}
 			}
